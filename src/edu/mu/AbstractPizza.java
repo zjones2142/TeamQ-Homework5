4c194ada513aafc8c3419f3ca484280f00872f5e
zjones2142 <zachjones2142@gmail.com>
2024-03-11 15:35:31 -0500

    zj - initial project setup

---
+++ src/edu/mu/AbstractPizza.java
@@ -0,0 +1,65 @@
+package edu.mu;
+
+import java.util.*;
+
+public abstract class AbstractPizza {
+
+	protected List<Toppings> toppingList;
+	protected double priceWithoutToppings;
+	protected double totalPrice;
+	protected int pizzaOrderID;
+	protected static int orderIDCounter;
+	protected ICookingStrategy cookingStrategy;
+	protected double cookingPrice;
+	
+	
+	//XXX: pdf says, "constructor should be implemented to instantiate the toppingList as an ArrayList"
+	//XXX: may need fix
+	protected AbstractPizza(List<Toppings> toppingList) {
+		super();
+		this.toppingList = toppingList;
+	}
+	
+	public List<Toppings> getToppingList() {
+		return toppingList;
+	}
+	public void setToppingList(List<Toppings> toppingList) {
+		this.toppingList = toppingList;
+	}
+	public double getPriceWithoutToppings() {
+		return priceWithoutToppings;
+	}
+	public void setPriceWithoutToppings(double priceWithoutToppings) {
+		this.priceWithoutToppings = priceWithoutToppings;
+	}
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public int getPizzaOrderID() {
+		return pizzaOrderID;
+	}
+	public void setPizzaOrderID(int pizzaOrderID) {
+		this.pizzaOrderID = pizzaOrderID;
+	}
+	public static int getOrderIDCounter() {
+		return orderIDCounter;
+	}
+	public static void setOrderIDCounter(int orderIDCounter) {
+		AbstractPizza.orderIDCounter = orderIDCounter;
+	}
+	public ICookingStrategy getCookingStrategy() {
+		return cookingStrategy;
+	}
+	public void setCookingStrategy(ICookingStrategy cookingStrategy) {
+		this.cookingStrategy = cookingStrategy;
+	}
+	public double getCookingPrice() {
+		return cookingPrice;
+	}
+	public void setCookingPrice(double cookingPrice) {
+		this.cookingPrice = cookingPrice;
+	}
+}
